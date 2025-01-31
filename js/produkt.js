e8a38eca6771778ed369f22a7953597aa45b1d5f
Mxdicc <jonathanskadhauge@gmail.com>
2025-01-31 08:37:32 +0100

    singleview

---
+++ js/produkt.js
@@ -0,0 +1,38 @@
+let produktid = 1526;
+let produktgrid = document.querySelector(".produktgrid");
+
+fetch(`https://kea-alt-del.dk/t7/api/products/${produktid}`)
+    .then(response => response.json())
+    .then(data => {
+        produktgrid.innerHTML = `
+        <div class="produktbillede">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${produktid}.webp" alt="">
+            </div>
+            <div class="produktinfo">
+                <p class="pi">Product Information</p>
+                <p class="mn">Model Name</p>
+                <p class="nm">${data.productdisplayname}</p>
+                <p class="cl">Color</p>
+                <p class="co">${data.basecolour}</p>
+                <p class="brand">${data.brandname} | ${data.articletype}</p>
+                <p class="in">Inventory number</p>
+                <p class="nu">${data.id}</p>
+                <p class="br">${data.brandname}</p>
+                <p class="ta">${data.brandbio}</p>
+            </div>
+            <div>
+                <p class="name">${data.productdisplayname}</p>
+                  <p class="mn">Price</p>
+                <p class="nm">DKK ${data.price},-</p>
+                
+                <div class="sizes">
+                    <p class="choose">Choose a size</p>
+                    <div class="list">
+                    <p class="size">S</p>
+                    <p calss="arrow">&#11206;</p>
+                    </div>
+                </div>
+                <p class="addto">Add To Basket</p>
+            </div>
+        `
+    })
