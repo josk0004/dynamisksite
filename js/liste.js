1fbcd032a9403bf6e59ae5610b4ad33e2ed3187b
Mxdicc <jonathanskadhauge@gmail.com>
2025-02-03 15:24:17 +0100

    add dynamics

--- js/liste.js
+++ js/liste.js
@@ -1,6 +1,10 @@
+const queryString = document.location.search;
+const URLparams = new URLSearchParams(queryString);
+const category = URLparams.get('category');
+console.log(category);
 const productList = document.querySelector('.produktliste');
 
-fetch('https://kea-alt-del.dk/t7/api/products?limit=10')
+fetch('https://kea-alt-del.dk/t7/api/products?limit=100&category=' + category)
     .then((response) => response.json())
     .then((data) => showList(data));
 
@@ -8,22 +12,22 @@ function showList(products) {
     console.log(products);
     const markup = products.map((product) =>
         ` <div class="produktkort">
-            <a href="produkt.html" class="${product.soldout === 1 ? 'grayscale' : ''}"><img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt=""></a>
+            <a href="produkt.html?id=${product.id}" class="${product.soldout === 1 ? 'grayscale' : ''}"><img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt=""></a>
 
-            <a href="produkt.html" class="npt">${product.productdisplayname}</a>
-            <a href="produkt.html" class="cat">${product.category}</a>
-            <a href="produkt.html" class="nowdkk">DKK ${product.price},-</a>
-            <a href="produkt.html" class="prevdkk ${product.discount}">DKK ${Math.round(product.price / (100 - product.discount) * 100)},-</a>
-            <a href="produkt.html" class="readmore">read more</a>
+            <a href="produkt.html?id=${product.id}" class="npt">${product.productdisplayname}</a>
+            <a href="produkt.html?id=${product.id}" class="cat">${product.category}</a>
+            <a href="produkt.html?id=${product.id}" class="nowdkk">DKK ${product.price},-</a>
+            <a href="produkt.html?id=${product.id}" class="prevdkk ${product.discount}">DKK ${Math.round(product.price / (100 - product.discount) * 100)},-</a>
+            <a href="produkt.html?id=${product.id}" class="readmore">read more</a>
 
             <div class="rabat ${product.discount}">
-                <a href="produkt.html">${product.discount}%</a>
+                <a href="produkt.html?id=${product.id}">${product.discount}%</a>
             </div>
             <div class="soldout ${product.soldout === 1 ? '' : 'null'}">
-                <a href="produkt.html">Sold Out</a>
+                <a href="produkt.html?id=${product.id}">Sold Out</a>
             </div>
         </div> `
     ).join("");
-    console.log(markup);
+    // console.log(markup);
     productList.innerHTML = markup;
 }
--- js/produkt.js
+++ js/produkt.js
@@ -1,12 +1,15 @@
-let produktid = 1526;
+const queryString = document.location.search;
+const URLparams = new URLSearchParams(queryString);
+const identity = URLparams.get('id');
+console.log(identity);
 const produktgrid = document.querySelector(".produktgrid");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/${produktid}`)
+fetch(`https://kea-alt-del.dk/t7/api/products/${identity}`)
     .then(response => response.json())
     .then(data => {
         produktgrid.innerHTML = `
         <div class="produktbillede">
-                <img src="https://kea-alt-del.dk/t7/images/webp/640/${produktid}.webp" alt="">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${identity}.webp" class="${data.soldout === 1 ? 'grayscale' : ''}" alt="">
             </div>
             <div class="produktinfo">
                 <p class="pi">Product Information</p>
@@ -24,6 +27,7 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${produktid}`)
                 <p class="brand">${data.brandname} | ${data.articletype}</p>
                 <p class="mn">Price</p>
                 <p class="nm">DKK ${data.price},-</p>
+                <p class="prevdkk ${data.discount}">DKK ${Math.round(data.price / (100 - data.discount) * 100)},-</p>
                 
                 <div class="sizes">
                     <p class="choose">Choose a size:</p>
